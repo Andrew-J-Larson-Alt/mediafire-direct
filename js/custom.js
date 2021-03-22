194254386779fdda3f8b3233fe050618d4759c2b
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-03-21 21:35:46 -0700

    Fixed issue upon loading page

--- js/custom.js
+++ js/custom.js
@@ -2,31 +2,35 @@
 
 let validMediafireFileDL = new RegExp('/https?:\/\/(www\.)?mediafire\.com\/file\/[a-zA-Z0-9]*\/file/gm');
 
-// Elements
+// Wait for page to load
 
-let inputMediafireURL = document.querySelector('#mediafire-url');
-let aMediafireDownloadBtn = document.querySelector('#mediafire-dl-btn');
-let pInvalidURL = document.querySelector('#invalid-url');
+window.addEventListener('load', function () {
+  // Elements
 
-// Main
+  let inputMediafireURL = document.querySelector('#mediafire-url');
+  let aMediafireDownloadBtn = document.querySelector('#mediafire-dl-btn');
+  let pInvalidURL = document.querySelector('#invalid-url');
 
-document.querySelector('input').addEventListener('change', function() {
-  let urlText = inputMediafireURL.value;
-  let validatedURL = validMediafireFileDL.test(urlText);
+  // Main
 
-  // Test if the new value is a valid link, to enable the download button
-  if (urlText) {
-    // check if we have valid url
-    if (validatedURL) {
-      if (aMediafireDownloadBtn.classList.contains('disable')) aMediafireDownloadBtn.classList.remove('disable');
-      if (pInvalidURL.classList.contains('hide')) pInvalidURL.classList.remove('hide');
+  document.querySelector('input').addEventListener('change', function() {
+    let urlText = inputMediafireURL.value;
+    let validatedURL = validMediafireFileDL.test(urlText);
+
+    // Test if the new value is a valid link, to enable the download button
+    if (urlText) {
+      // check if we have valid url
+      if (validatedURL) {
+        if (aMediafireDownloadBtn.classList.contains('disable')) aMediafireDownloadBtn.classList.remove('disable');
+        if (pInvalidURL.classList.contains('hide')) pInvalidURL.classList.remove('hide');
+      } else {
+        if (!aMediafireDownloadBtn.classList.contains('disable')) aMediafireDownloadBtn.classList.add('disable');
+        if (pInvalidURL.classList.contains('hide')) pInvalidURL.classList.remove('hide');
+      }
     } else {
+      // need to reset when no text is entered
       if (!aMediafireDownloadBtn.classList.contains('disable')) aMediafireDownloadBtn.classList.add('disable');
-      if (pInvalidURL.classList.contains('hide')) pInvalidURL.classList.remove('hide');
+      if (!pInvalidURL.classList.contains('hide')) pInvalidURL.classList.add('hide');
     }
-  } else {
-    // need to reset when no text is entered
-    if (!aMediafireDownloadBtn.classList.contains('disable')) aMediafireDownloadBtn.classList.add('disable');
-    if (!pInvalidURL.classList.contains('hide')) pInvalidURL.classList.add('hide');
-  }
+  });
 });
