9334044c8dd357d7c3d95a9cf3d3becc9e1053e2
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-03-21 22:12:29 -0700

    Changed how the timeout works

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,37 @@
 // Constants
 
 let validMediafireFileDL = /https?:\/\/(www\.)?mediafire\.com\/file\/[a-zA-Z0-9]*\/file/gm;
+let validateDelayCheck = null;
 
 // Functions
 
 let validationChecker = function(url, dlBtn, pInvalid) {
-  let validatedURL = validMediafireFileDL.test(url);
-
-  // Test if the new value is a valid link, to enable the download button
-  if (url) {
-    // check if we have valid url
-    if (validatedURL) {
-      if (dlBtn.classList.contains('disable')) dlBtn.classList.remove('disable');
-      if (!pInvalid.classList.contains('hide')) pInvalid.classList.add('hide');
-    } else {
-      if (!dlBtn.classList.contains('disable')) dlBtn.classList.add('disable');
-      if (pInvalid.classList.contains('hide')) pInvalid.classList.remove('hide');
-      }
-  } else {
-    // need to reset when no text is entered
-    if (!dlBtn.classList.contains('disable')) dlBtn.classList.add('disable');
-    if (!pInvalid.classList.contains('hide')) pInvalid.classList.add('hide');
+  // clear previous timeout
+  if (validateDelayCheck) {
+    clearTimeout(validateDelayCheck);
+    validateDelayCheck = null;
   }
+
+  // start new timeout
+  validateDelayCheck = setTimeout(function() {
+    let validatedURL = validMediafireFileDL.test(url);
+
+    // Test if the new value is a valid link, to enable the download button
+    if (url) {
+      // check if we have valid url
+      if (validatedURL) {
+        if (dlBtn.classList.contains('disable')) dlBtn.classList.remove('disable');
+        if (!pInvalid.classList.contains('hide')) pInvalid.classList.add('hide');
+      } else {
+        if (!dlBtn.classList.contains('disable')) dlBtn.classList.add('disable');
+        if (pInvalid.classList.contains('hide')) pInvalid.classList.remove('hide');
+      }
+    } else {
+      // need to reset when no text is entered
+      if (!dlBtn.classList.contains('disable')) dlBtn.classList.add('disable');
+      if (!pInvalid.classList.contains('hide')) pInvalid.classList.add('hide');
+    }
+  }, 100);
 };
 
 // Wait for page to load
@@ -37,8 +47,8 @@ window.addEventListener('load', function () {
   // need 100 ms delay to get true value afterwards
 
   // detect key presses
-  inputMediafireURL.addEventListener('keyup', function() {setTimeout(function() {validationChecker(inputMediafireURL.value || '', aMediafireDownloadBtn, pInvalidURL)}, 100)});
+  inputMediafireURL.addEventListener('keyup', function() {validationChecker(inputMediafireURL.value || '', aMediafireDownloadBtn, pInvalidURL)});
   // detect right-click actions
-  inputMediafireURL.addEventListener('oncut', function() {setTimeout(function() {validationChecker(inputMediafireURL.value || '', aMediafireDownloadBtn, pInvalidURL)}, 100)});
-  inputMediafireURL.addEventListener('onpaste', function() {setTimeout(function() {validationChecker(inputMediafireURL.value || '', aMediafireDownloadBtn, pInvalidURL)}, 100)});
+  inputMediafireURL.addEventListener('oncut', function() {validationChecker(inputMediafireURL.value || '', aMediafireDownloadBtn, pInvalidURL)});
+  inputMediafireURL.addEventListener('onpaste', function() {validationChecker(inputMediafireURL.value || '', aMediafireDownloadBtn, pInvalidURL)});
 });
