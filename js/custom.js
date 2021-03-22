46520d659df7c2751bc95cb84b5e213fa8156405
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-03-21 21:52:47 -0700

    Fixed function typo

--- js/custom.js
+++ js/custom.js
@@ -6,24 +6,23 @@ let validMediafireFileDL = /https?:\/\/(www\.)?mediafire\.com\/file\/[a-zA-Z0-9]
 
 let validationChecker = function() {
   let urlText = inputMediafireURL.value || '';
-    let validatedURL = validMediafireFileDL.test(urlText);
+  let validatedURL = validMediafireFileDL.test(urlText);
 
-    // Test if the new value is a valid link, to enable the download button
-    if (urlText) {
-      // check if we have valid url
-      if (validatedURL) {
-        if (aMediafireDownloadBtn.classList.contains('disable')) aMediafireDownloadBtn.classList.remove('disable');
-        if (!pInvalidURL.classList.contains('hide')) pInvalidURL.classList.add('hide');
-      } else {
-        if (!aMediafireDownloadBtn.classList.contains('disable')) aMediafireDownloadBtn.classList.add('disable');
-        if (pInvalidURL.classList.contains('hide')) pInvalidURL.classList.remove('hide');
-      }
-    } else {
-      // need to reset when no text is entered
-      if (!aMediafireDownloadBtn.classList.contains('disable')) aMediafireDownloadBtn.classList.add('disable');
+  // Test if the new value is a valid link, to enable the download button
+  if (urlText) {
+    // check if we have valid url
+    if (validatedURL) {
+      if (aMediafireDownloadBtn.classList.contains('disable')) aMediafireDownloadBtn.classList.remove('disable');
       if (!pInvalidURL.classList.contains('hide')) pInvalidURL.classList.add('hide');
-    }
-  });
+    } else {
+      if (!aMediafireDownloadBtn.classList.contains('disable')) aMediafireDownloadBtn.classList.add('disable');
+      if (pInvalidURL.classList.contains('hide')) pInvalidURL.classList.remove('hide');
+      }
+  } else {
+    // need to reset when no text is entered
+    if (!aMediafireDownloadBtn.classList.contains('disable')) aMediafireDownloadBtn.classList.add('disable');
+    if (!pInvalidURL.classList.contains('hide')) pInvalidURL.classList.add('hide');
+  }
 };
 
 // Wait for page to load
