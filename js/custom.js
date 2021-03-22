e2d8a0c366580afb57e9eea4ab51dba0ae9ccb83
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-03-22 03:42:56 -0700

    Fixes one more issue with files maybe?

--- js/custom.js
+++ js/custom.js
@@ -34,6 +34,7 @@ function downloadFile(filePath){
   let link=document.createElement('a');
   link.href = filePath;
   link.download = filePath.substr(filePath.lastIndexOf('/') + 1);
+  link.target = '_blank';
   link.click();
 }
 
