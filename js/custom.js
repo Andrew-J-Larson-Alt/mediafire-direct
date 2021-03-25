5be4d57e2775f814244e32c9ff5a6e9632a9f02b
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-03-25 02:30:16 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -59,7 +59,7 @@ function downloadFileStarting() {
       else if (isFirefox) window.location = 'about:newtab';
       else window.location = 'about:blank';
     }
-  }, paramDownloadDelay);
+  }, paramDownloadDelay*5);
 } 
 function downloadFileBegin(filePath) {
   let iframeDivDL = document.createElement('div');
