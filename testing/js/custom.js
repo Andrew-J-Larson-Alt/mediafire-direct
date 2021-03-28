28c532a2c36d63980181c82f349a639c40fbbc6a
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-03-28 14:11:03 -0700

    testing one step at a time

--- testing/js/custom.js
+++ testing/js/custom.js
@@ -1,1 +1,11 @@
-console.log('testing');
+// Constants
+
+const corsProxy = 'https://api.allorigins.win/get?url=';
+const validMediafireIdentifierDL = true;
+const validMediafireShortDL = true;
+const validMediafireViewDL = true;
+const validMediafireFileDL = true;
+const checkHTTP = false;
+const paramDL_initialDelay = 50; // ms
+const paramDL_loadDelay = 750; // ms
+
