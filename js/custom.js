e06970ab0206e6e2550f1e229af9ffaac3edc874
Andrew Larson <19640692+TheAlienDrew@users.noreply.github.com>
2021-03-28 13:29:04 -0700

    Isn't functions in here causing problems
    Most likely the issue is something to do with jquery???

--- js/custom.js
+++ js/custom.js
@@ -12,7 +12,7 @@ const paramDL_loadDelay = 750; // ms
 // Browser Detection Variables
 var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 var isFirefox = typeof InstallTrigger !== 'undefined';
-var isSafari = false;//TESTING<<<<<<< /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
 var isIE = /*@cc_on!@*/false || !!document.documentMode;
 var isEdge = !isIE && !!window.StyleMedia;
 var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
