db96d79cfbb8d30673e850b27c8bbce3cd363ada
Andrew Larson (Alt) <github-alt@drewj.la>
2025-02-09 07:30:01 -0800

    Old alt email update
    Fixed website inconsistencies, 'disabled' links to websites that no longer exist or aren't updated anymore, and this is the last time I'm updating the github alt contact email.

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2023  Andrew Larson (andrew.j.larson18+github+alt@gmail.com)
+/* Copyright (C) 2023  Andrew Larson (github-alt@drewj.la)
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
